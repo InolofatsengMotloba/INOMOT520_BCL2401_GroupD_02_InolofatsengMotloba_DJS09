d5be8e53db52040c46cef5eda31db70ae16984a5
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-11 09:56:05 +0200

    Add an array type to the you object

--- src/index.ts
+++ src/index.ts
@@ -44,14 +44,14 @@ const you: {
   lastName: string;
   isReturning: boolean;
   age: number;
+  stayedAt: string[];
 } = {
   firstName: "Bobby",
   lastName: "Brown",
   isReturning: true,
   age: 35,
+  stayedAt: ["florida-home", "oman-flat", "tokyo-bungalow"],
 };
-
-console.log(you.userName);
 
 function populateUser(isReturning: boolean, userName: string) {
   if (isReturning) {
@@ -60,4 +60,4 @@ function populateUser(isReturning: boolean, userName: string) {
   userNameDisplay.innerHTML = userName;
 }
 
-populateUser(you.isReturning, you.userName);
+populateUser(you.isReturning, you.firstName);
