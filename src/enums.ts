3feb11414a3912312b88e04381dd01ac6f46b09c
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-12 09:31:28 +0200

    Add comments to enums.ts

--- src/enums.ts
+++ src/enums.ts
@@ -1,8 +1,10 @@
+// Export an enumeration to define user permissions levels
 export enum Permissions {
   ADMIN = "ADMIN",
   READ_ONLY = "READ_ONLY",
 }
 
+// Export an enumeration to define different loyalty user tiers
 export enum LoyaltyUser {
   GOLD_USER = "GOLD_USER",
   SILVER_USER = "SILVER_USER",
