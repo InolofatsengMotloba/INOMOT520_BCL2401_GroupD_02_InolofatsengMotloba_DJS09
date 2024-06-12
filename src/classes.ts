62ba027827e0438a64b798c49df0c50697321bed
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-12 09:30:01 +0200

    Add comments to classes.ts

--- src/classes.ts
+++ src/classes.ts
@@ -1,9 +1,13 @@
+// Import the Review interface from the interfaces module
 import { Review } from "./interfaces";
 
+// Export the MainProperty class as the default export of this module
 export default class MainProperty {
   src: string;
   title: string;
   reviews: Review[];
+
+  // Constructor to initialize the properties of the MainProperty class
   constructor(src: string, title: string, reviews: Review[]) {
     this.src = src;
     this.title = title;
