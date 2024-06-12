48ae79e3adda71868ba3e8508b0ef6809ad6edcd
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-12 09:35:36 +0200

    Add comments to types.ts

--- src/types.ts
+++ src/types.ts
@@ -1,3 +1,3 @@
-// type Alias
+// Define a type alias for the Price & Country to represent possible values
 export type Price = 25 | 30 | 35 | 45;
 export type Country = "Colombia" | "Poland" | "United Kingdom" | "Malaysia";
