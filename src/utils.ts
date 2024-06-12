0a6002583448c77a5ef11f731163f1b7278e2e48
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-12 09:39:50 +0200

    Add comments to utils.ts

--- src/utils.ts
+++ src/utils.ts
@@ -1,19 +1,13 @@
+// Import the Review interface from the interfaces module
 import { Review } from "./interfaces";
+import { Permissions, LoyaltyUser } from "./enums";
 
+// Select DOM elements
 const reviewTotalDisplay = document.querySelector("#reviews");
 const returningUserDisplay = document.querySelector("#returning-user");
 const userNameDisplay = document.querySelector("#user");
 
-enum Permissions {
-  ADMIN = "ADMIN",
-  READ_ONLY = "READ_ONLY",
-}
-
-enum LoyaltyUser {
-  GOLD_USER = "GOLD_USER",
-  SILVER_USER = "SILVER_USER",
-  BRONZE_USER = "BRONZE_USER",
-}
+// Function to display the total number of reviews along with the last reviewer's name and an icon
 export function showReviewTotal(
   value: number,
   reviewer: string,
@@ -30,6 +24,7 @@ export function showReviewTotal(
     iconDisplay;
 }
 
+// Function to display a message for returning users and to show the user's name
 export function populateUser(isReturning: boolean, userName: string) {
   if (isReturning == true) {
     returningUserDisplay.innerHTML = "back";
@@ -37,6 +32,7 @@ export function populateUser(isReturning: boolean, userName: string) {
   userNameDisplay.innerHTML = userName;
 }
 
+// Function to show the price per night if the value is true or if the user has appropriate permissions
 export function showDetails(
   value: boolean | Permissions,
   element: HTMLDivElement,
@@ -49,12 +45,14 @@ export function showDetails(
   }
 }
 
+// Function to return 's' if the value is greater than 1 or equal to 0 (for pluralizing 'review')
 export function makeMultiple(value: number): string {
   if (value > 1 || value == 0) {
     return "s";
   } else return "";
 }
 
+// Function to get the top two reviews based on the number of stars
 export function getTopTwoReviews(reviews: Review[]): Review[] {
   const sortedReviews = reviews.sort((a, b) => b.stars - a.stars);
   return sortedReviews.slice(0, 2);
