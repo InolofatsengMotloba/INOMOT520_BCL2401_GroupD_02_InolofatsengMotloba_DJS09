6e2c375aebd5f3a0edc3272b68dd43e240470044
Inolofatseng Motloba <motlobainolo123@gmail.com>
2024-06-12 09:15:59 +0200

    Tidy Up code by storing classes in separate file

--- src/index.ts
+++ src/index.ts
@@ -8,6 +8,7 @@ import {
 } from "./utils";
 import { Permissions, LoyaltyUser } from "./enums";
 import { Review, Property } from "./interfaces";
+import MainProperty from "./classes"
 
 let isLoggedIn: boolean;
 
@@ -148,18 +149,6 @@ footer.innerHTML =
   " " +
   currentLocation[2] +
   "°";
-
-// Main Property
-class MainProperty {
-  src: string;
-  title: string;
-  reviews: Review[];
-  constructor(src: string, title: string, reviews: Review[]) {
-    this.src = src;
-    this.title = title;
-    this.reviews = reviews;
-  }
-}
 
 let yourMainProperty = new MainProperty(
   "./images/italian-property.jpg",
